43b27b9c0676c976583595fd00023214a8f93d05
sotreduch2602 <sotreduch26022001.com>
2025-06-18 20:46:29 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', function () {
+    echo "Test route is working!";
+});
+
+Route::get('a/b/c/d/e/f', function () {
+    $name = 'Nguyen Van A';
+    echo "<h2>Hello, $name!</h2>";
+});
