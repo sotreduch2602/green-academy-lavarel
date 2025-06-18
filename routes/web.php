22da8b26ecb6e3295cdb5b802b96a7239fce9f47
sotreduch2602 <sotreduch26022001.com>
2025-06-18 21:08:45 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@ Route::get('a/b/c/d/e/f', function () {
     $name = 'Nguyen Van A';
     echo "<h2>Hello, $name!</h2>";
 });
+
+Route::get('name/{name}', function ($name) {
+    echo $name;
+});
+
+Route::get('product/detail/{id}', function ($id) {
+    echo "Product ID: $id";
+});
+
+Route::get('ten/{ten?}/tuoi/{tuoi?}', function ($ten = 'Anonymous', $tuoi = 'Unknown') {
+    echo "Name: $ten, Age: $tuoi";
+});
+
+Route::get('test/{test?}', function ($test = 'null'){
+    echo "Test value: $test";
+});
