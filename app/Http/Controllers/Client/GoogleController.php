cbe16861a0a4922787457ccc4865c9cfa45eeb64
sotreduch2602 <sotreduch26022001.com>
2025-07-07 19:09:01 +0700

    Refactor Google login callback to handle existing users and add Google ID to user model

--- app/Http/Controllers/Client/GoogleController.php
+++ app/Http/Controllers/Client/GoogleController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Client;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
@@ -17,15 +17,23 @@ class GoogleController extends Controller
     public function callback(){
         $googleUser = Socialite::driver('google')->user();
 
-        $user = User::create([
-            'name' => $googleUser->getName(),
-            'email' => $googleUser->getEmail(),
-            'password' => Hash::make(uniqid()), // Generate a random password
-            'google_id' => $googleUser->getId(), // Store Google ID if needed
-        ]);
+        $user = User::where('google_id', $googleUser->getId())->first();
+
+        if (!$user){
+            $user = User::create([
+                'name' => $googleUser->getName(),
+                'email' => $googleUser->getEmail(),
+                'password' => Hash::make(uniqid()), // Generate a random password
+                'google_id' => $googleUser->getId(), // Store Google ID if needed
+            ]);
+        }
+        else{
+            $user->name = $googleUser->getName();
+            $user->save();
+        }
 
         Auth::login($user);
 
-        return redirect()->route();
+        return redirect(route('admin.product_category.list', absolute: false));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::prefix('admin/product_category')
+->controller(ProductCategoryController::class)
+->name('admin.product_category.')
+->group(function(){
+    Route::get('list', 'list')->name('list');
+    Route::post('store', 'store')->name('store');
+    Route::get('create', 'create')->name('create');
+    Route::get('make_slug', 'makeSlug')->name('make_slug');
+    Route::post('destroy/{id}', 'destroy')->name('destroy');
+    Route::get('detail/{id}', 'detail')->name('detail');
+    Route::post('update/{id}', 'update')->name('update');
 });
 
 require __DIR__.'/auth.php';
